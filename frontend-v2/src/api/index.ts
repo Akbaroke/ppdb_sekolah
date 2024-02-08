681cdf69e985c4b79efb470580a191cdaeec2f7b
akbaroke <akbarmuhammad833@gmail.com>
2024-02-08 16:51:49 +0700

    update: remove withCredential

--- frontend-v2/src/api/index.ts
+++ frontend-v2/src/api/index.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_APP_URL,
-  withCredentials: true,
 });
 
 // Tambahkan interceptor untuk menambahkan token ke setiap permintaan
